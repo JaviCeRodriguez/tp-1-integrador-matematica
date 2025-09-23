3372a4e8b7102adce5d9c234b97035f1bcfee327
jerepaez99 <155403722+jerepaez99@users.noreply.github.com>
2025-09-23 19:44:00 -0300

    operaciones booleanas: validador
    Agregada una función que checkea que se estén introduciendo solo 0 y 1. Caso contrario, salta error

--- op_booleanas_jere.py
+++ op_booleanas_jere.py
@@ -1,46 +1,59 @@
+def validador(*variables):
+    for v in variables:
+        if v not in (0,1):
+            raise ValueError(f"Se esperaba 0 o 1 y se introdujo {v}")
+
 def AND(a,b):
+    validador(a,b)
     if a == 1 and b == 1:
         return 1
     else:
         return 0
     
 def OR(a,b):
+    validador(a,b)
     if a == 1 or b == 1:
         return 1
     else:
         return 0
     
 def NOT(a):
+    validador(a)
     if a == 0:
         return 1
     else: 
         return 0
     
 def IMPL(a,b):
+    validador(a,b)
     if a == 1 and b == 0:
         return 0
     else:
         return 1
 
 def NOR(a,b):
+    validador(a,b)
     if OR(a,b) == 1:
         return 0
     else:
         return 1
 
 def NAND(a,b):
+    validador(a,b)
     if AND(a,b) == 1:
         return 0
     else:
         return 1
     
 def XOR(a,b):
+    validador(a,b)
     if a != b:
         return 1
     else:
         return 0
 
 def XNOR(a,b):
+    validador(a,b)
     if a == b:
         return 1
     else:
