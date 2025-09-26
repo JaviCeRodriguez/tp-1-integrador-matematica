589d33c1d776eb35936816056d966b2cf2855cbc
JaviCeRodriguez <javicerodriguez@gmail.com>
2025-09-26 18:46:17 -0300

    add: rest of code

--- helpers/Validador_imput.py
+++
@@ -1,8 +0,0 @@
-"Pedir a usuario que escriba la función booleana."
-"Validar que las operaciones booleanas sean válidas (solo se permite ^, v, ~, => y <=>)."
-"Si está todo ok, se da feedback al usuario de que se está analizando la función. Caso contrario, que vuelva a escribir la función."
-
-def validacion_de_funciones():
-    operadores = ['^', 'v', '~', '=>', '<=>'] #operadores permitidos
-    fun_bool = (input("Escriba la función booleana, usando los siguientes operadores: ^, v, ~, =>, <=>: ")) #(a^b) v (~c), por ejemplo
-    
